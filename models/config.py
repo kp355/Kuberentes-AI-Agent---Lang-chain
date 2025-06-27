ce39ba264e5b196ed4e402151eb6006774c55253
syedumarfarooq <syedumarfarooq7403@gmail.com>
2025-06-28 00:54:20 +0530

    additional changes to the refactored code

--- models/config.py
+++ models/config.py
@@ -4,11 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not set!")
-
-if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("Missing Supabase credentials")
